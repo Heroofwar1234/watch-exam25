2150054083df8a574f3ee3f2e3356c0f71656712
Joey <joeyalcocer@outlook.com>
2025-06-12 12:54:13 -0600

    Agregar Pausas

--- lib/jswatch_web/statecharts/clock_manager.ex
+++ lib/jswatch_web/statecharts/clock_manager.ex
@@ -1,6 +1,9 @@
 defmodule JswatchWeb.ClockManager do
   use GenServer
 
+
+  @ciclable [:Day, :Month, :Year]
+
   def format_date(date, show, selection) do
     day = if date.day < 10, do: "0#{date.day}", else: "#{date.day}"
     month = ~w[ENE FEB MAR ABR MAY JUN JUL AGO SEP OCT NOV DIC] |> Enum.at(date.month - 1)
@@ -23,8 +26,8 @@ defmodule JswatchWeb.ClockManager do
     Process.send_after(self(), :working_working, 1000)
     GenServer.cast(ui, {:set_time_display, Time.truncate(time, :second) |> Time.to_string })
     GenServer.cast(ui, {:set_date_display, format_date(date, true, Day) })
-    {:ok, %{ui_pid: ui, time: time, date: date, alarm: alarm, st1: Working, st2: Idle}}
-  end
+    {:ok, %{ui_pid: ui, time: time, date: date, alarm: alarm, st1: Working, st2: Idle, date_edit: false, date_field: Day}}
+  end   #Agregar Campos para parar y seleccionar campo para cambiar , default es day
 
   def handle_info(:working_working, %{ui_pid: ui, time: time, alarm: alarm, st1: Working} = state) do
     Process.send_after(self(), :working_working, 1000)
@@ -37,4 +40,24 @@ defmodule JswatchWeb.ClockManager do
   end
 
   def handle_info(_event, state), do: {:noreply, state}
+
+  # Camviar a modo editar
+  def handle_info(:"top-left-pressed", %{date_edit: false} = state) do
+    GenServer.cast(state.ui_pid, {:set_date_display, format_date(state.date, true, Day)})
+    {:noreply, %{state | date_edit: true, date_field: Day}}
+    #cambia date_edit a true
+  end
+   def handle_info(:"bottom-left-pressed", %{date_edit: true} = state) do
+    GenServer.cast(state.ui_pid, {:set_date_display, format_date(state.date, true, Day)})
+    {:noreply, %{state | date_edit: false}}
+    #Cambia a falso
+
+  end
+
+    # Ciclar campo por cambiar
+  def handle_info(:"bottom-right-pressed", %{date_edit: true, date_field: current} = state) do
+    next = Enum.at(@ciclable, rem(Enum.find_index(@ciclable, &(&1 == current)) + 1, 3))
+    GenServer.cast(state.ui_pid, {:set_date_display, format_date(state.date, true, next)})
+    {:noreply, %{state | date_field: next}}
+  end
 end
